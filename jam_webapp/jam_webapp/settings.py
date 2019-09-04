c9f5867e9e721e5261eeda553a659f2456bc0b82
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-09-03 21:42:40 -0400

    Implemented basic chat with django channels

--- jam_webapp/jam_webapp/settings.py
+++ jam_webapp/jam_webapp/settings.py
@@ -121,5 +121,12 @@ STATIC_URL = '/static/'
 
 # Channels
 ASGI_APPLICATION = 'jam_webapp.routing.application'
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
--- jam_webapp/jamsession/consumers.py
+++ jam_webapp/jamsession/consumers.py
@@ -1,17 +1,44 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.session_name = self.scope['url_route']['kwargs']['jam_session_name']
+        self.session_group_name = 'jam-session_%s' % self.session_name
+
+        # join session group
+        async_to_sync(self.channel_layer.group_add)(
+            self.session_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # leave session group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.session_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.session_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+
+        # send message to websocket
         self.send(text_data=json.dumps({
             'message': message
         }))
